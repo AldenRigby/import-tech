2bfdc83db93edea1a530e83b86e37acf448b4c04
AldenRigby <alden.rigby@ucas-edu.net>
2024-11-05 11:27:38 -0700

    awef

---
+++ main.py
@@ -0,0 +1,9 @@
+from tech import Phone, Laptop
+
+phone = Phone("iPhone XXXIV", 1, "greenish blue")
+laptop = Laptop("Dell", 976324735763254, 1)
+
+print(phone)
+print(laptop)
+print(repr(phone))
+print(repr(laptop))
